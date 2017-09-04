eb1b8cf6c097a10a8a941216534120733d8612e9
Lukas <l_moeller@gmx.at>
2017-09-04 02:07:49 +0200

    test

--- test.py
+++ test.py
@@ -1,15 +1,11 @@
-import pygame
-from pygame.locals import *
-pygame.init()
-WIDTH = 1280
-HEIGHT = 1080
-windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
-img = pygame.image.load("myimage.jpg")
-while True:
-        events = pygame.event.get()
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-        windowSurface.blit(img, (0, 0)) #Replace (0, 0) with desired coordinates
-        pygame.display.flip()
+from tkinter import *
+
+def main():
+    root = Tk()
+    canvas = Canvas(root, width =1224,height=1000)
+    logo=PhotoImage(file="17_03 Barcelona Marathon/DS_0339.jpg")
+    canvas.create_image(0, 0, image=logo) #Change 0, 0 to whichever coordinates you need
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
